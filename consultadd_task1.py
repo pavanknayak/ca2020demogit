8c4be560e755f83b0b5555fe88b78b68e786d252
pnayak <pavanknayak@gmail.com>
2020-04-02 19:03:13 -0700

    added task 1 answers

---
+++ consultadd_task1.py
@@ -0,0 +1,52 @@
+#ConsultAdd task 1
+
+#Question 1
+a, b, c = 10, 15.7, "here is a string"
+
+#Question 2
+
+comp = -1**0.5
+integ = 12
+
+comp = integ +3
+
+#Question 3
+
+var1 = 2
+var2 = 3
+var3 = var1 * var2
+
+var4 = 4
+var5 = 5
+
+var5 = var5 - 1
+
+#Question 4
+
+def printy(inp):
+    print(inp)
+    
+#Question 5
+    
+def value():
+    inp1 = input("input a number between 1-10")
+    inp2 = input("input another number between 1-10")
+    
+    z = inp1 + inp2
+    z = z + 30
+    print(z)
+    
+#Question 6
+
+def vtype(inpu):
+    print(type(inpu))
+    
+#Question 7
+
+CamVar = 10
+ladVar = 11
+UPVAR = 12
+
+#Question 8
+
+#Yes the value will change because you are overwriting the memory space for that variable with a new data type
